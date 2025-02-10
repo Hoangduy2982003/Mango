102c5793e20ccf803672657316356ec1ed3a4db6
Hoangduy2982003 <125873702+Hoangduy2982003@users.noreply.github.com>
2025-02-10 13:37:42 +0700

    Add Auth Service in Web Project

--- Mango.Web/Utility/SD.cs
+++ Mango.Web/Utility/SD.cs
@@ -4,6 +4,7 @@ public class SD
 {
     //Base URL coupon API
     public static string CouponAPIBase { get; set; }
+    public static string AuthAPIBase { get; set; }
     public enum ApiType
     {
         GET,

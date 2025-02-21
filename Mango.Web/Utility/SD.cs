5d75e108da4e565d75654c78371afd6fd5ad84c3
Hoangduy2982003 <125873702+Hoangduy2982003@users.noreply.github.com>
2025-02-21 23:42:39 +0700

    Finish Product API

--- Mango.Web/Utility/SD.cs
+++ Mango.Web/Utility/SD.cs
@@ -4,6 +4,7 @@ public class SD
 {
     //Base URL coupon API
     public static string CouponAPIBase { get; set; }
+    public static string ProductAPIBase { get; set; }
     public static string AuthAPIBase { get; set; }
     public const string RoleAdmin = "ADMIN";
     public const string RoleCustomer = "CUSTOMER";
